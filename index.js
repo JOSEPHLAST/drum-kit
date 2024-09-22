66401517fb3d349edd5711f5ae546aa05ed0a491
Josephlast <asikatochukwu1@gmail.com>
2024-09-22 04:19:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+for (i = 0; i < document.querySelectorAll('.drum').length; i++) {
+
+document.querySelectorAll('button')[i].addEventListener('click', function () {
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+});
+
+}
+
+document.addEventListener('keydown', function (event) {
+  makeSound(event.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+      case 'w':
+        new Audio('sounds/crash.mp3').play();
+        break;
+
+      case 'a':
+        new Audio('sounds/kick-bass.mp3').play();
+        break;
+
+      case 's':
+        new Audio('sounds/snare.mp3').play();
+        break;
+
+      case 'd':
+        new Audio('sounds/tom-1.mp3').play();
+        break;
+
+      case 'j':
+        new Audio('sounds/tom-2.mp3').play();
+        break;
+
+      case 'k':
+        new Audio('sounds/tom-3.mp3').play();
+        break;
+
+      case 'l':
+        new Audio('sounds/tom-4.mp3').play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+
+    }
+}
